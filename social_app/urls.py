b4a2cd4bd43ca3312b239afd49d56f3bf8a43deb
Ricardo Branco <ricardo.vezetiv@mail.ru>
2022-05-24 16:16:34 -0300

    Criada página de perfil individual, conectando a página que lista todos os perfis.

--- social_app/urls.py
+++ social_app/urls.py
@@ -6,4 +6,5 @@ app_name = "social_app"
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
     path("profile_list/", views.profile_list, name="profile_list"),
+    path("profile/<int:pk>", views.profile, name="profile"),
 ]
--- social_app/views.py
+++ social_app/views.py
@@ -9,3 +9,8 @@ def dashboard(request):
 def profile_list(request):
     profiles = Profile.objects.exclude(user=request.user)
     return render(request, "social_app/partials/profile_list.html", {"profiles": profiles})
+
+
+def profile(request, pk):
+    profile = Profile.objects.get(pk=pk)
+    return render(request, "social_app/partials/profile.html", {"profile": profile})
