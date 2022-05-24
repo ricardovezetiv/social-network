132e7f0ce1f6a98401bdef851181997f03119f0b
Ricardo Branco <ricardo.vezetiv@mail.ru>
2022-05-24 16:36:41 -0300

    Criada a rotina de 'Follow' and 'Unfollow' para outros perfis.

--- social_app/views.py
+++ social_app/views.py
@@ -12,5 +12,21 @@ def profile_list(request):
 
 
 def profile(request, pk):
+    if not hasattr(request.user, 'profile'):
+        missing_profile = Profile(user=request.user)
+        missing_profile.save()
+
     profile = Profile.objects.get(pk=pk)
+
+    if request.method == "POST":
+        current_user_profile = request.user.profile
+        data = request.POST
+        action = data.get("follow")
+
+        if action == "follow":
+            current_user_profile.follows.add(profile)
+        elif action == "unfollow":
+            current_user_profile.follows.remove(profile)
+
+        current_user_profile.save()
     return render(request, "social_app/partials/profile.html", {"profile": profile})
