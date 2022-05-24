bdcc6b350474556b0ef7460854f3dbe3a1badb43
Ricardo Branco <ricardo.vezetiv@mail.ru>
2022-05-24 12:02:09 -0300

    Implementado um gancho após o salvamento.

--- social_app/models.py
+++ social_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User
 
 
@@ -10,3 +12,15 @@ class Profile(models.Model):
         symmetrical=False,
         blank=True,
     )
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        user_profile.follows.add(instance.profile)
+        user_profile.save()
